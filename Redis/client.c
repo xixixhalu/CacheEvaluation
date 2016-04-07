8c5028273b11b4c4a8a723f23d888306f619d0fd
xixixhalu <xixixhalu@gmail.com>
2016-04-07 11:56:49 -0700

    complete the tiny tiny client for first testing

--- Redis/client.c
+++ Redis/client.c
@@ -1,14 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <hiredis/hiredis.h>
 
+redisContext *c;
+redisReply *reply;
+
+/*-------Test Functions-------------*/
+void singleInsert();
+void multiInsert();
+
+/*-------Utility Functions----------*/
+void initialize(int argc, char **argv);
+void pingServer();
+bool setData(char* key, size_t ksize, char* value, size_t vsize);
+
 int main(int argc, char **argv) {
-    unsigned int j;
-    redisContext *c;
-    redisReply *reply;
-    const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
+    
+    initialize(argc, argv);
+
+    pingServer();
+	
+	//singleInsert();
+	multiInsert();
+
+    /* Disconnects and frees the context */
+    redisFree(c);
+
+    return 0;
+}
+
+/*-------Test Functions-------------*/
+void singleInsert() {
+	int i = 0;
+	char key[10];
+	char value[10];
+	sprintf(key, "%d", i);
+	sprintf(value, "%d", i);
+	if (!setData(key, 10, value, 990)) {
+		printf("Cannot insert Key: %d\n", i);
+	}
+}
+
+void multiInsert() {
+	int i;
+	for (i = 0; i < 1000; i++) {
+		char key[10];
+		char value[10];
+		sprintf(key, "%d", i);
+		sprintf(value, "%d", i);
+		if (!setData(key, 10, value, 990)) {
+			printf("Cannot insert Key: %d\n", i);
+			break;
+		}
+	}
+}
+
+/*-------Utility Functions-------------*/
+
+/* Initialize Client */
+void initialize(int argc, char **argv) {
+	const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
     int port = (argc > 2) ? atoi(argv[2]) : 6379;
 
     struct timeval timeout = { 1, 500000 }; // 1.5 seconds
@@ -22,24 +76,30 @@ int main(int argc, char **argv) {
         }
         exit(1);
     }
+}
 
-    /* PING server */
+/* PING server */
+void pingServer() {
     reply = redisCommand(c,"PING");
     printf("PING: %s\n", reply->str);
     freeReplyObject(reply);
+}
 
-    /* Set a key */
+/* Set a key using binary safe API */
+bool setData(char* key, size_t ksize, char* value, size_t vsize) {
+	/* Set a key */
+	/*
     reply = redisCommand(c,"SET %s %s", "foo", "hello world");
     printf("SET: %s\n", reply->str);
     freeReplyObject(reply);
-
+	*/
     /* Set a key using binary safe API */
-    reply = redisCommand(c,"SET %b %b", "bar", (size_t) 3, "hello", (size_t) 5);
+    reply = redisCommand(c,"HSET user: %b %b", key, ksize, value, vsize);
     printf("SET (binary API): %s\n", reply->str);
+	if (reply->type == REDIS_REPLY_ERROR) {
+		freeReplyObject(reply);
+		return false;
+	}
     freeReplyObject(reply);
-
-    /* Disconnects and frees the context */
-    redisFree(c);
-
-    return 0;
+	return true;
 }
---
+++ Redis/start_redis.sh
@@ -0,0 +1,4 @@
+gnome-terminal -x redis-cli
+cd /home/bowen/Documents/Projects/CacheEvaluation/Redis/config
+redis-server redis.conf
+
